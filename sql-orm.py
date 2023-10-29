b23dd24e24ea15497b4bf2887aad4ab790d7aca5
Michelle Britton <michelle@daisy-webdesign.co.uk>
2023-10-29 17:58:26 +0000

    commit

--- sql-orm.py
+++ sql-orm.py
@@ -2,7 +2,7 @@ from sqlalchemy import (
     create_engine, Column, Float, ForeignKey, Integer, String
 )
 from sqlalchemy.ext.declarative import declarative_base
-from sql-alchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker
 
 # executing the instructions from the "chinook" database
 db = create_engine("postgresql:///chinook")
@@ -38,9 +38,6 @@ class Track(base):
     Bytes = Column(Integer, primary_key=False)
     UnitPrice = Column(Float)
 
-
-
-
 # instead of connecting to the database directly, we will ask for a session
 # create a new instance of sessionmaker, then point to our engine (the db)
 Session = sessionmaker(db)
@@ -56,3 +53,40 @@ base.metadata.create_all(db)
 # Now that we have the file set up, we can start to build our class based models, but this time we get to simply build a normal Python object, that subclasses base
 # The class based models need to be added before the session is created above but after the base is declared since we need to use the base subclass.
 
+# Query 1 - select all records from the "Artist" table
+#artists = session.query(Artist)
+# for artist in artists:
+#     print(artist.ArtistId, artist.Name, sep=" | ")
+
+# Query 2 - select only the "Name" column from the "Artist" table
+# artists = session.query(Artist)
+# for artist in artists:
+#     print(artist.Name)
+
+# Query 3 - select only "Queen" from the "Artist" table
+# artist = session.query(Artist).filter_by(Name="Queen").first()
+# print(artist.ArtistId, artist.Name, sep=" | ")
+
+# Query 4 - select only by "ArtistId" #51 from the "Artist" table
+# artist = session.query(Artist).filter_by(ArtistId=51).first()
+# print(artist.ArtistId, artist.Name, sep=" | ")
+
+# Query 5 - select only the albums with "ArtistId" #51 from the "Album" table
+# albums = session.query(Album).filter_by(ArtistId=51)
+# for album in albums:
+#     print(album.AlbumId, album.Title, album.ArtistId, sep=" | ")
+
+# Query 6 - Select all tracks where the composer is "Queen" from the track table
+tracks = session.query(Track).filter_by(Composer="Queen")
+for track in tracks:
+    print(
+        track.TrackId,
+        track.Name,
+        track.AlbumId,
+        track.GenreId,
+        track.Composer,
+        track.Milliseconds,
+        track.Bytes,
+        track.UnitPrice,
+        sep=" | "
+    )
